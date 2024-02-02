5179610ffc6ee995764d2daf9fa6c4efc34a276f
Ashish Gupta <ashishgupt25@gmail.com>
2024-02-02 16:15:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="m-0 p-0 box-sizing: border-box w-screen h-screen">
+            <div className="w-3/6 h-2/5 bg-orange-700 mt-52 ml-44"></div>
+        </div>
+    );
+}
+
+export default App;
