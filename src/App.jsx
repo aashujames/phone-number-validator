28c98b3cd0029b9b1fe0e0717ad4f09ffb6df09d
Ashish Gupta <ashishgupt25@gmail.com>
2024-02-06 15:54:37 +0530

    adding react-international-phone UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import MuiPhone from "./components/MuiPhone";
+
 function App() {
+    const [phone, setPhone] = useState("+1");
+
     return (
         <div className="m-0 p-0 box-sizing: border-box w-screen h-screen">
-            <div className="w-3/6 h-2/5 bg-orange-700 mt-52 ml-44"></div>
+            <div className="w-3/6 h-2/5  mt-52 ml-72 flex flex-col items-center justify-evenly">
+                <div className="font-bold">Try the Phone Number Validator</div>
+                <div>
+                    Enter a phone number below to see how our phone validation
+                    software works.
+                </div>
+                <div>
+                    <MuiPhone value={phone} onChange={setPhone} />
+                </div>
+                <div>d</div>
+            </div>
         </div>
     );
 }
